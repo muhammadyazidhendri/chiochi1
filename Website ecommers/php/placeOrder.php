c446cca086ab5669e19ab50a47e995009f7a47af
Muhammad Nurul Adhi <muhammadnuruladhi@gmail.com>
2025-01-09 12:42:06 +0700

    mencoba ganti phpnya

--- Website ecommers/php/placeOrder.php
+++ Website ecommers/php/placeOrder.php
@@ -20,28 +20,33 @@ require_once dirname(__FILE__) . '/midtrans-php-master/Midtrans.php';
 // Set 3DS transaction for credit card to true
 \Midtrans\Config::$is3ds = true;
 
-// Ambil data JSON yang dikirim
-$data = json_decode(file_get_contents('php://input'), true);
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        // Ambil data JSON yang dikirim
+    $data = json_decode(file_get_contents('php://input'), true);
 
-// Pastikan data diterima
-if (isset($data['total']) && isset($data['items']) && isset($data['name']) && isset($data['email']) && isset($data['phone'])) {
- // Lanjutkan dengan proses pembayaran
- $params = array(
-    'transaction_details' => array(
-        'order_id' => rand(),
-        'gross_amount' => $data['total'],
-    ),
-    'item_details' => $data['items'],
-    'customer_details' => array(
-        'first_name' => $data['name'],
-        'email' => $data['email'],
-        'phone' => $data['phone'],
-    ),
-);
+    // Pastikan data diterima
+    if (isset($data['total']) && isset($data['items']) && isset($data['name']) && isset($data['email']) && isset($data['phone'])) {
+    // Lanjutkan dengan proses pembayaran
+    $params = array(
+        'transaction_details' => array(
+            'order_id' => rand(),
+            'gross_amount' => $data['total'],
+        ),
+        'item_details' => $data['items'],
+        'customer_details' => array(
+            'first_name' => $data['name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+        ),
+    );
 
-$snapToken = \Midtrans\Snap::getSnapToken($params);
-echo $snapToken; // Kirim token kembali ke JavaScript
+    $snapToken = \Midtrans\Snap::getSnapToken($params);
+    echo $snapToken; // Kirim token kembali ke JavaScript
+    } else {
+        echo 'Data tidak lengkap'; // Tampilkan pesan error jika data tidak lengkap
+    }
 } else {
-    echo 'Data tidak lengkap'; // Tampilkan pesan error jika data tidak lengkap
+    http_response_code(405);
+    echo 'method tidak di kenalins';
 }
 ?>
